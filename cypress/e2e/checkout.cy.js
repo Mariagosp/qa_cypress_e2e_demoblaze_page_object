9a4fac7b6a08c4016775cae4e8b6ac1b13cf8879
Maria <m.gosp1704@gmail.com>
2025-03-27 12:15:41 +0200

    solution

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,39 @@
 /// <reference types='cypress' />
+import HomeAndCataloguePageObject
+  from '../support/pages/homeCatalogue.pageObject';
+
+const homePage = new HomeAndCataloguePageObject();
 
 describe('', () => {
   before(() => {
-
+    homePage.visit();
   });
 
-  it('', () => {
+  it('should provide the ability to add item to a cart', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
 
+    homePage.clickOnAddToCart();
+
+    homePage.assertAllert('Product added');
+
+    homePage.clickOnLink('Cart');
+
+    homePage.assertProductInCart('Sony vaio i7');
+
+    homePage.clickOnButton();
+
+    homePage.typeName('Mariia');
+    homePage.typeCountry('Ukraine');
+    homePage.typeCity('Kyiv');
+    homePage.typeCard('1234567890');
+    homePage.typeMonth('April');
+    homePage.typeYear('2025');
+
+    homePage.clickOnPurchaseButton();
+
+    homePage.assertDataInModal('Mariia', '1234567890');
+
+    homePage.clickOnConfirmButton();
   });
 });
--- cypress/support/pages/homeCatalogue.pageObject.js
+++ cypress/support/pages/homeCatalogue.pageObject.js
@@ -3,19 +3,105 @@ import PageObject from '../PageObject';
 class HomeAndCataloguePageObject extends PageObject {
   url = '/index.html';
 
+  get buttonClassBtn() {
+    return cy.get('.btn-success');
+  }
+
+  get addToCartBtn() {
+    return cy.get('.btn-success');
+  }
+
+  get purchaseBtn() {
+    return cy.contains('.btn', 'Purchase');
+  }
+
+  get confirmBtn() {
+    return cy.get('.confirm');
+  }
+
+  get nameField() {
+    return cy.get('#name');
+  }
+
+  get countryField() {
+    return cy.get('#country');
+  }
+
+  get cityField() {
+    return cy.get('#city');
+  }
+
+  get cardField() {
+    return cy.get('#card');
+  }
+
+  get monthField() {
+    return cy.get('#month');
+  }
+
+  get yearField() {
+    return cy.get('#year');
+  }
+
+  typeName(name) {
+    this.nameField.type(name);
+  }
+
+  typeCountry(country) {
+    this.countryField.type(country);
+  }
+
+  typeCity(city) {
+    this.cityField.type(city);
+  }
+
+  typeCard(card) {
+    this.cardField.type(card);
+  }
+
+  typeMonth(month) {
+    this.monthField.type(month);
+  }
+
+  typeYear(year) {
+    this.yearField.type(year);
+  }
+
   clickOnLink(linkName) {
-    cy.contains('.nav-link', linkName)
-      .click();
+    cy.contains('.nav-link', linkName).click();
   }
 
   clickOnCategory(categoryName) {
-    cy.contains('#itemc', categoryName)
-      .click();
+    cy.contains('#itemc', categoryName).click();
   }
 
   clickOnProduct(product) {
-    cy.contains('.hrefch', product)
-      .click();
+    cy.contains('.hrefch', product).click();
+  }
+
+  clickOnButton() {
+    this.buttonClassBtn.click();
+  }
+
+  clickOnAddToCart() {
+    this.addToCartBtn.click();
+  }
+
+  clickOnPurchaseButton() {
+    this.purchaseBtn.click();
+  }
+
+  clickOnConfirmButton() {
+    this.confirmBtn.click();
+  }
+
+  assertProductInCart(product) {
+    cy.contains(product).should('be.visible');
+  }
+
+  assertDataInModal(name, card) {
+    cy.contains(name).should('be.visible');
+    cy.contains(card).should('be.visible');
   }
 }
 
